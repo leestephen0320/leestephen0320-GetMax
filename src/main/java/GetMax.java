c7a15a02354e57752f9920cab5443bc38e4e688a
leestephen0320 <leestephen0320@gmail.com>
2023-02-13 04:08:59 +0000

    modified:   src/main/java/GetMax.java

--- src/main/java/GetMax.java
+++ src/main/java/GetMax.java
@@ -7,6 +7,12 @@ public class GetMax {
      * @return the largest value in arr.
      */
     public int max(int[] arr){
-        return -1;
+        int a;
+        for(int i = 0; i < arr.length; i++){
+            if(a < arr[i]){
+                a = arr[i];
+            }
+        }
+        return a;
     }
 }
